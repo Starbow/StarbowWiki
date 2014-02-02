24b562846b11acf8ee0c5004254207a4b1344128
Graylin Kim <graylin.kim@gmail.com>
2014-02-02 15:06:47 -0500

    Initial commit of Persona v0.5 extension.

---
+++ extensions/Persona/Persona.i18n.php
@@ -0,0 +1,274 @@
+<?php
+
+/**
+ * This file is part of the MediaWiki extension Persona.
+ * Copyright (C) 2012 Tyler Romeo <tylerromeo@gmail.com>
+ *
+ * Extension:Persona is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * Extension:Persona is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with Extension:Persona.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+$messages = array();
+
+$messages['en'] = array(
+	'persona-login' => 'Login with Persona',
+	'persona-desc' => 'Allows users to log in with their Mozilla Persona account.'
+);
+
+/** Message documentation (Message documentation)
+ * @author Beta16
+ * @author Shirayuki
+ */
+$messages['qqq'] = array(
+	'persona-login' => 'Label for the Persona login link and button',
+	'persona-desc' => '{{desc|name=Persona|url=http://www.mediawiki.org/wiki/Extension:Persona}}',
+);
+
+/** Asturian (asturianu)
+ * @author Xuacu
+ */
+$messages['ast'] = array(
+	'persona-login' => 'Aniciar sesión con Persona',
+	'persona-desc' => 'Permite que los usuarios anicien sesión cola so cuenta de Mozilla Persona',
+);
+
+/** Belarusian (Taraškievica orthography) (беларуская (тарашкевіца)‎)
+ * @author Wizardist
+ */
+$messages['be-tarask'] = array(
+	'persona-login' => 'Увайсьці праз Persona',
+	'persona-desc' => 'Дазваляе ўваходзіць праз рахунак у Mozilla Persona.',
+);
+
+/** Czech (česky)
+ * @author Mormegil
+ */
+$messages['cs'] = array(
+	'persona-login' => 'Přihlásit pomocí Persona',
+	'persona-desc' => 'Umožňuje uživatelům přihlásit se pomocí účtu Mozilla Persona.',
+);
+
+/** German (Deutsch)
+ * @author Metalhead64
+ */
+$messages['de'] = array(
+	'persona-login' => 'Mit Persona anmelden',
+	'persona-desc' => 'Ermöglicht es Benutzern, sich mit ihrem Mozilla-Persona-Konto anzumelden',
+);
+
+/** Lower Sorbian (dolnoserbski)
+ * @author Michawiki
+ */
+$messages['dsb'] = array(
+	'persona-login' => 'Pśizjawjenje z Persona',
+	'persona-desc' => 'Zmóžnja wužywarjam se z jich kontom Mozilla Persona pśizjawiś.',
+);
+
+/** Spanish (español)
+ * @author Armando-Martin
+ */
+$messages['es'] = array(
+	'persona-login' => 'Iniciar sesión con Persona',
+	'persona-desc' => 'Permite a los usuarios iniciar sesión con su cuenta de Mozilla Persona',
+);
+
+/** Finnish (suomi)
+ * @author Silvonen
+ */
+$messages['fi'] = array(
+	'persona-login' => 'Kirjaudu Persona-tunnuksella',
+);
+
+/** French (français)
+ * @author Gomoko
+ */
+$messages['fr'] = array(
+	'persona-login' => 'Connexion avec Persona',
+	'persona-desc' => 'Permet aux utilisateurs de se connecter avec leur compte Persona de Mozilla.',
+);
+
+/** Galician (galego)
+ * @author Toliño
+ */
+$messages['gl'] = array(
+	'persona-login' => 'Rexistro con Persona',
+	'persona-desc' => 'Permite aos usuarios rexistrarse coa súa conta de Mozilla Persona',
+);
+
+/** Hebrew (עברית)
+ * @author Amire80
+ */
+$messages['he'] = array(
+	'persona-login' => 'כניסה באמצעות פרסונה',
+	'persona-desc' => 'הוספת אפשרות להיכנס באמצעות חשבון מוזילה פרסונה.',
+);
+
+/** Upper Sorbian (hornjoserbsce)
+ * @author Michawiki
+ */
+$messages['hsb'] = array(
+	'persona-login' => 'Přizjewjenje z Persona',
+	'persona-desc' => 'Zmóžnja wužiwarjam so z jich kontom Mozilla Persona přizjewić.',
+);
+
+/** Italian (italiano)
+ * @author Beta16
+ */
+$messages['it'] = array(
+	'persona-login' => 'Accedi con Persona',
+	'persona-desc' => "Permette agli utenti di effettuare l'accesso tramite la loro l'utenza Mozilla Persona.",
+);
+
+/** Japanese (日本語)
+ * @author Shirayuki
+ */
+$messages['ja'] = array(
+	'persona-login' => 'Persona でログイン',
+	'persona-desc' => '利用者が自分の Mozilla Persona アカウントでログインできるようにする。',
+);
+
+/** Korean (한국어)
+ * @author 아라
+ */
+$messages['ko'] = array(
+	'persona-login' => 'Persona로 로그인',
+	'persona-desc' => '사용자가 자신의 Mozilla Persona 계정으로 로그인할 수 있습니다',
+);
+
+/** Colognian (Ripoarisch)
+ * @author Purodha
+ */
+$messages['ksh'] = array(
+	'persona-login' => 'Met <i lang="en>Persona</i> enlogge',
+	'persona-desc' => 'Löht Metmaacher övver iehre Zohjang zom <i lang="en">Persona</i> vun Mozilla enlogge.',
+);
+
+/** Luxembourgish (Lëtzebuergesch)
+ * @author Robby
+ */
+$messages['lb'] = array(
+	'persona-login' => 'Mat Persona aloggen',
+	'persona-desc' => 'Erlaabt et Benotzer mat hirem Mozilla-Persona Benotzerkont anzeloggen',
+);
+
+/** Minangkabau (Baso Minangkabau)
+ * @author Iwan Novirion
+ */
+$messages['min'] = array(
+	'persona-login' => 'Login jo Persona',
+	'persona-desc' => 'Izinkan pangguno untuak login jo akun Mozilla Persona.',
+);
+
+/** Macedonian (македонски)
+ * @author Bjankuloski06
+ */
+$messages['mk'] = array(
+	'persona-login' => 'Најава со Persona',
+	'persona-desc' => 'Овозможува корисниците да се најавуваат со нивната сметка на Mozilla Persona.',
+);
+
+/** Dutch (Nederlands)
+ * @author Siebrand
+ * @author Wiki13
+ */
+$messages['nl'] = array(
+	'persona-login' => 'Aanmelden met Persona',
+	'persona-desc' => 'Maakt het mogelijk om gebruikers aan te laten melden met hun Mozilla Personagebruiker.',
+);
+
+/** Piedmontese (Piemontèis)
+ * @author Borichèt
+ * @author Dragonòt
+ */
+$messages['pms'] = array(
+	'persona-login' => 'Intré ant ël sistema con Persona',
+	'persona-desc' => "A përmëtt a j'utent d'intré ant ël sistema con sò cont Mozilla Persona.",
+);
+
+/** Portuguese (português)
+ * @author Fúlvio
+ */
+$messages['pt'] = array(
+	'persona-login' => 'Entrar com Persona',
+	'persona-desc' => 'Permite que os utilizadores iniciem sessão com sua conta do Mozilla Persona.',
+);
+
+/** Romansh (rumantsch)
+ * @author Gion-andri
+ */
+$messages['rm'] = array(
+	'persona-login' => "S'annunziar cun Persona",
+	'persona-desc' => "Lubescha ad utilisaders da s'annunziar cun lur contos da Mozilla Persona.",
+);
+
+/** Romanian (română)
+ * @author Minisarm
+ */
+$messages['ro'] = array(
+	'persona-login' => 'Autentificare cu Persona',
+);
+
+/** tarandíne (tarandíne)
+ * @author Joetaras
+ */
+$messages['roa-tara'] = array(
+	'persona-login' => 'Tràse cu Persona',
+	'persona-desc' => "Permette a l'utinde de trasè cu 'u cunde lore de Mozilla Persona.",
+);
+
+/** Russian (русский)
+ * @author Okras
+ */
+$messages['ru'] = array(
+	'persona-login' => 'Войти, используя Persona',
+	'persona-desc' => 'Позволяет пользователям входить с помощью их учётной записи в Mozilla Persona.',
+);
+
+/** Sinhala (සිංහල)
+ * @author පසිඳු කාවින්ද
+ */
+$messages['si'] = array(
+	'persona-login' => 'පුද්ගලතාව සමඟ ප්‍රවිෂ්ට වන්න',
+);
+
+/** Swedish (svenska)
+ * @author WikiPhoenix
+ */
+$messages['sv'] = array(
+	'persona-login' => 'Logga in med Persona',
+	'persona-desc' => 'Låter användare att logga in med sitt Mozilla Persona-konto.',
+);
+
+/** Ukrainian (українська)
+ * @author Ата
+ */
+$messages['uk'] = array(
+	'persona-login' => 'Увійти, використовуючи Persona',
+	'persona-desc' => 'Дозволяє користувачам входити з допомогою облікового запису Mozilla Persona.',
+);
+
+/** Simplified Chinese (中文（简体）‎)
+ * @author Yfdyh000
+ */
+$messages['zh-hans'] = array(
+	'persona-login' => '用 Persona 登录',
+	'persona-desc' => '允许用户使用他们的 Mozilla Persona 帐户登录。',
+);
+
+/** Traditional Chinese (中文（繁體）‎)
+ * @author Justincheng12345
+ */
+$messages['zh-hant'] = array(
+	'persona-login' => '以Persona登入',
+	'persona-desc' => '容許用戶使用Mozilla Persona帳戶登入。',
+);
